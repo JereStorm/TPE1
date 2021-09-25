b9994abd0566de6d5195bcde5bebbc8855544a40
Jeremias Storm <jerestorm@gmail.com>
2021-09-25 17:30:45 -0300

    preparado para agregar base de datos

--- model/ProductsModel.php
+++ model/ProductsModel.php
@@ -5,7 +5,7 @@ class ProductsModel{
     private $db;
 
     public function __construct(){
-        $this -> db = new PDO('mysql:host=localhost;'.'dbname=db_universidad;charset=utf8', 'root', '');
+        $this -> db = new PDO('mysql:host=localhost;'.'dbname=;charset=utf8', 'root', '');
     }
 
     
