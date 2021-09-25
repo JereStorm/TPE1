68d57652865b9e5857089da845b410daa0a993a3
Tomas Cogliatti <tomas.cogliatti@gmail.com>
2021-09-25 15:27:01 -0300

    probando git

--- router.php
+++ router.php
@@ -5,6 +5,7 @@ require_once ('controller/ProductsController.php');
 // defino la base url para la construccion de links con urls semánticas
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
+//probando
 
 if (!empty($_GET['action'])){
     $action = $_GET['action'];
