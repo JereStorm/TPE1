abeb5d404dbcdd90dc05d7e5a70d03dcea4a23ce
Jeremias Storm <jerestorm@gmail.com>
2021-10-04 12:07:32 -0300

    tire el comando pull

--- templates_c/faed5dcc43424f2d9498b9a24bffb949aad34866_0.file.formEdit.tpl.php
+++ templates_c/faed5dcc43424f2d9498b9a24bffb949aad34866_0.file.formEdit.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-09-28 18:26:16
+/* Smarty version 3.1.39, created on 2021-10-04 17:02:42
   from 'C:\xampp\htdocs\proyectos\WEB2\TPE\01tpe\templates\forms\formEdit.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_615342281d70f5_94682435',
+  'unifunc' => 'content_615b1792188400_64379874',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'faed5dcc43424f2d9498b9a24bffb949aad34866' => 
     array (
       0 => 'C:\\xampp\\htdocs\\proyectos\\WEB2\\TPE\\01tpe\\templates\\forms\\formEdit.tpl',
-      1 => 1632846362,
+      1 => 1633359505,
       2 => 'file',
     ),
   ),
@@ -20,9 +20,10 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
     'file:forms/cuerpoProd.tpl' => 1,
     'file:forms/cuerpoType.tpl' => 1,
+    'file:forms/cuerpoStock.tpl' => 1,
   ),
 ),false)) {
-function content_615342281d70f5_94682435 (Smarty_Internal_Template $_smarty_tpl) {
+function content_615b1792188400_64379874 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!-- formulario de alta de tarea -->
 <form action="edit/<?php echo $_smarty_tpl->tpl_vars['URL']->value;?>
 " method="POST" class="my-4">
@@ -33,6 +34,9 @@ function content_615342281d70f5_94682435 (Smarty_Internal_Template $_smarty_tpl)
 ?>
     <?php } elseif ($_smarty_tpl->tpl_vars['URL']->value == 'TipoProducto') {?>
         <?php $_smarty_tpl->_subTemplateRender("file:forms/cuerpoType.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+    <?php } elseif ($_smarty_tpl->tpl_vars['URL']->value == 'Stock') {?>
+        <?php $_smarty_tpl->_subTemplateRender("file:forms/cuerpoStock.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
     <?php }?>
 </form><?php }
