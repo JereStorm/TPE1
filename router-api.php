d543fa39043647118d8a958436a5fbf0ca172089
Jeremias Storm <jerestorm@gmail.com>
2021-10-27 23:25:38 -0300

    plantilla terminada

--- router-api.php
+++ router-api.php
@@ -5,9 +5,15 @@ require_once 'api/apiProductsController.php';
 
 $router = new Router();
 
-// tabla de ruteo
-$router->addRoute('Producto', 'GET', 'apiProductsController', 'getAll');
-$router->addRoute('Producto/:ID', 'GET', 'apiProductsController', 'getOne');
+//creamos el router
+$router = new Router();
+
+//definimos la tabla (  $router->addRoute(RECURSO, METODO, CLASSE, FUNCION)  )
+$router->addRoute('coments', 'GET', 'ApiComentsController', 'getAll');
+$router->addRoute('coments:ID', 'GET', 'ApiComentsController', 'getOne');
+$router->addRoute('coments:ID', 'DELETE', 'ApiComentsController', 'delete');
+//$router->addRoute('productos', 'POST', 'ApiComentsController', 'add');
+//$router->addRoute('productos/:ID', 'UPDATE', 'ApiComentsController', 'update');
 
 // ruteo
 $resource = $_GET['resource'];
